f8e984b00ab27a4fbc8e0cb70dfde5e0fef95123
gabridp <dirtec@frest3001.com.br>
2022-03-31 11:45:12 -0300

    first commit

---
+++ AgendaWeb.Presentation/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaWeb.Presentation.Controllers
+{
+    [Authorize]
+    public class UsuarioController : Controller
+    {
+        //método para abrir a página 'MinhaConta'
+        public IActionResult MinhaConta()
+        {
+            return View();
+        }
+    }
+}
+
+
